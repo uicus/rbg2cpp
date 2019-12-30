3deddc65354ed013e751afb56fe0cc91e23cfd5e
MSZ <marek.esz@gmail.com>
2019-12-30 21:00:11 +0100

    Example benchmark script.

--- example_benchmark.sh
+++ example_benchmark.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 # Example flat-MC benchmark with 1s per game.
 
-cd rbg2cpp
 mkdir logs
 
 ./run_benchmark.sh amazons 1000
 ./run_benchmark.sh breakthrough 1000
 ./run_benchmark.sh chess 1000
-./run_benchmark.sh englishCheckers 1000
+./run_benchmark.sh englishDraughts 1000
 ./run_benchmark.sh hex 1000
-./run_benchmark.sh internationalCheckers 1000
+./run_benchmark.sh internationalDraughts 1000
 ./run_benchmark.sh skirmish 1000
